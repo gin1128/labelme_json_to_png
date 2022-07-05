c7f43a84c86ec45f323d36307a158549a1883934
ginpei <yahagi.ginpei@isid.co.jp>
2022-07-05 14:28:24 +0900

    fix: 白黒に変換する処理を追記

--- labelme_json_to_png.py
+++ labelme_json_to_png.py
@@ -5,6 +5,8 @@ import argparse
 import tqdm
 from PIL import Image
 import numpy as np
+import glob
+
 
 TEMP_DIR = "temp"
 
@@ -23,7 +25,7 @@ assert json_dir_path.exists(), "JSON directory is not exist."
 assert out_dir_path.exists(), "Output directory is not exist."
 assert label_file_path.exists(), "Label file is not exist."
 
-# Preprocessing.
+# Preprocessing.（ラベルの割り当て、tempディレクトリの作成）
 correct_label_dict = {}
 with open(str(label_file_path), "r") as file:
     for i, line in enumerate(file):
@@ -34,6 +36,7 @@ if not temp_dir_path.exists():
 
 # Batch process Converting json to png.
 for json_file_path in tqdm.tqdm(list(json_dir_path.glob("*.json"))):
+    
     # Execute "labelme_json_to_dataset".
     subprocess.run(["labelme_json_to_dataset", str(json_file_path), "-o", str(temp_dir_path)], stdout=subprocess.PIPE,
                    stderr=subprocess.PIPE, text=True)
@@ -52,9 +55,11 @@ for json_file_path in tqdm.tqdm(list(json_dir_path.glob("*.json"))):
             label_indexes[label_name] = (image_array == label_num)
 
         for label_name, label_index in label_indexes.items():
+            
             correct_label_num = correct_label_dict[label_name]
             image_array[label_index] = correct_label_num
 
+    # image_array = (image_array > 128) * 255
     new_image = Image.fromarray(image_array, mode="P")
     new_image.putpalette(origin_color_palette)
     new_image.save(str(out_dir_path.joinpath(json_file_path.name).with_suffix(".png")))
@@ -63,4 +68,13 @@ for json_file_path in tqdm.tqdm(list(json_dir_path.glob("*.json"))):
 if temp_dir_path.exists():
     shutil.rmtree(str(temp_dir_path))
 
+# 白黒の変換
+image_path_list = glob.glob("output/*")
+for path in image_path_list:
+    img = np.array(Image.open(path))
+    img = np.where(img > 0.5, 255,0)
+    new_image = Image.fromarray(img.astype(np.uint8))
+    new_image.save(path)
+
 print("Conversion is finished.")
+
